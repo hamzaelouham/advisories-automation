94d0236bed1d7aa8f60c5f64aa0197ef453b723f
hamzaeelouham <hamzaelouham@gmail.com>
2024-01-22 23:25:30 +0000

    using session

--- oraclelinux.py
+++ oraclelinux.py
@@ -1,17 +1,16 @@
 from datetime import datetime, timezone
 import time
-import requests
+import requests 
 import pandas as pd
 import logging
 from bs4 import BeautifulSoup
-import urllib.parse as url_tools 
-from dateutil.relativedelta import relativedelta
+# import urllib.parse as url_tools 
+# from dateutil.relativedelta import relativedelta
 
 # Configure the loggin
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 log = logging.getLogger("logger")
-
+request = requests.Session()
 headers = {
    "User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
 }
@@ -22,7 +21,7 @@ def get_html(url):
     try:
         log.info('Sending request to URL: %s', url)
         initialtime  = time.time()
-        response = requests.get(url,headers=headers)
+        response = request.get(url,headers=headers)
         
         if response.status_code == 200 or response.status_code == 201:
             log.info('Request successful. Status code: %d. Time taken: %.2f seconds', response.status_code,time.time() - initialtime)
@@ -30,14 +29,14 @@ def get_html(url):
         else:
             log.error('Unexpected status code: %d. Time taken: %.2f seconds', response.status_code, time.time() - initialtime)
             return
-    except requests.exceptions.RequestException as error:
+    except request.exceptions.RequestException as error:
         log.error('Error during request: %s', error)
         return
 
     
            
 def scrape(link, version):
-
+    
     # Oracle Linux 9 (x86_64)
     html = get_html(link) 
     soup = BeautifulSoup(html,features='html.parser')
