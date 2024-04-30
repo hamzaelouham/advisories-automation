887166de63af5084df2c686641cca61aecefa839
hamza elouham <hamzaelouham@gmail.com>
2024-04-30 17:46:43 +0100

    working on  tested ..

--- atos-rating.py
+++ atos-rating.py
@@ -24,29 +24,39 @@ def read_execl(filename):
     
 
     
-def update_row(row):
-     pass
+def save(df,f):
+    log.info('saving ...!')
+    file_name = f'{f}-Tested.xlsx'
+    path = os.path.join("Tested", file_name)
+    if not os.path.exists("Tested"):
+       os.makedirs("Tested")
+    df.to_excel(path, index=False) 
 
 def main():
     initialtime  = time.time()
-    filter = str(sys.argv[1])
-    
-    print()
+    filter = str(sys.argv[1]) 
+    # RHEL 7
+    execl_name =  str(sys.argv[2])
+    text_file_name = str(sys.argv[3]) 
+
     log.info('starting process of rating ...!')
-    
-    execl_name = or 'Redhat-Generated-Month-March.xlsx'
-    text_file_name = 'redhat7.txt'
+   
+    old_excel = read_execl(execl_name)
+    old_excel = old_excel[old_excel['OS'] == filter]
+    tested_rpms = set(read_file(text_file_name))
+        # for line in read_file(text_file_name):
 
-    old_execl = read_execl(execl_name)
+    for index, row in old_excel.iterrows():
+        if row['RPMs'] in tested_rpms:
+            log.info('founding Tested package !')
+            old_excel.at[index, 'Tested'] = "YES"
+            #   old_excel.at[index, 'Tested'] = "YES"
+            print(f'{row["RPMs"]} is Tested')
 
-    for index, row in old_execl.iterrows():
-        for line in read_file(text_file_name):
-               if row['RPMs'] == line:
-                  log.info('founding Tested package !')
-                  print(f'{line} is Tested')
+    save(old_excel, filter)
+    log.info('successful finishing. Time taken: %.2f seconds' ,time.time() - initialtime)                  
                   
                      
-    log.info('successful finishing. Time taken: %.2f seconds' ,time.time() - initialtime)                  
 
 
 if __name__ == "__main__":
