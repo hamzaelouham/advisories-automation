10727548b9468cb4890ce6969c630331f0179214
hamza elouham <hamzaelouham@gmail.com>
2024-05-13 15:53:34 +0100

    ubuntu done

--- ubuntu.py
+++ ubuntu.py
@@ -1,12 +1,15 @@
+from dateutil.relativedelta import relativedelta
 from datetime import datetime, timezone
-import time
+from bs4 import BeautifulSoup
+import pandas as pd
+import calendar
 import requests
 import logging
-import pandas as pd
-from bs4 import BeautifulSoup
-from dateutil.relativedelta import relativedelta
+import asyncio
+import httpx
+import time
 import os
-
+import re
 
 
 # Configure the loggin
@@ -22,15 +25,40 @@ ubuntu = ('Ubuntu 22.04', 'Ubuntu 20.04')
 
 patching_date = datetime.now(timezone.utc) - relativedelta(months=1)
 
+# headers = {
+#    "User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+# }
+
 headers = {
-   "User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,fr;q=0.8,fr-FR;q=0.7,en-GB;q=0.6",
+    "cache-control": "max-age=0",
+    "priority": "u=0, i",
+    "sec-ch-ua": "\"Chromium\";v=\"124\", \"Microsoft Edge\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
 }
+async def  _get_html(url):
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            if response.status_code == 200:
+                return response.text
+            else:
+                print(f"Request failed with status code: {response.status_code}")
+    except httpx.RequestError as exc:
+        print(f"An error occurred during the request: {exc}")
 
 def get_html(url):
     try:
         log.info('Sending request to URL: %s', url)
         initialtime  = time.time()
-        response = http.get(url,headers=headers)
+        response = http.get(url,headers=headers,allow_redirects=True)
         
         if response.status_code == 200 or response.status_code == 201:
             log.info('Request successful. Status code: %d. Time taken: %.2f seconds', response.status_code,time.time() - initialtime)
@@ -41,6 +69,17 @@ def get_html(url):
     except requests.exceptions.RequestException as error:
         log.error('Error during request: %s', error)
         return
+
+def skip_link(link):
+    if re.search(r'/LSN-\d+-\d+$', link):
+            return True
+    return False
+
+def is_within_month(parsed_date, patching_date):
+     patching_month_start = datetime(patching_date.year, patching_date.month, 1).date()
+     last_day =  calendar.monthrange(patching_date.year, patching_date.month)[1]
+     patching_month_end = datetime(patching_date.year, patching_date.month, last_day).date()
+     return patching_month_start <= parsed_date <= patching_month_end
 
 def parse_date2(date_str):
     return datetime.strptime(date_str, '%d %B %Y').date() 
@@ -74,7 +113,7 @@ def extract_links(current_offset=0):
         pub_date = article.find('p', class_="u-no-margin u-no-padding--top")
         if pub_date:
             parsed_date = parse_date2(pub_date.text.strip())
-            if parsed_date >= datetime(2024, int(patching_date.strftime("%m")), 1).date():
+            if is_within_month(parsed_date, patching_date):
                 links.append(url + article.find('a')['href'])
             else:
                 return links    
@@ -82,24 +121,42 @@ def extract_links(current_offset=0):
     return links + extract_links(next_offset) 
 
 
-def extract_pages(links):
+async def extract_pages(links):
     big_data = []
     for link in links:
-        html = get_html(link)  
+        if skip_link(link):
+           log.warning('skiping this link : %s',link) 
+           continue 
+        # html = await _get_html(link)  
         # ["latin-1", "iso-8859-1", "windows-1251"]).unicode_markup ,from_encoding=html.encoding
-        soup = BeautifulSoup(html,features='lxml')
-        section = soup.find('section',class_="p-strip--suru-topped")
+        # soup = BeautifulSoup(html,features='lxml')
+        # section = soup.find('section',class_="p-strip--suru-topped")
+        max_retries = 10
+        retry_count = 0
+        while retry_count < max_retries:
+            html = await _get_html(link)
+            soup = BeautifulSoup(html, features='lxml')
+            section = soup.find('section', class_="p-strip--suru-topped")
+            if section:
+                break  # Exit loop if section is found
+            else:
+                retry_count += 1
+                log.warning(f"Retrying... (Attempt {retry_count}/{max_retries}")
+                
+        if not section:
+            print(link)
         title = products = None
         if section:
             div_fixed = section.find('div', class_="u-fixed-width")
             rows = section.find_all('div', class_="row")
             if div_fixed and rows:
                title_element = div_fixed.find("h1")
-               issue_date = parse_date(div_fixed.find('p',class_="p-muted-heading").text.strip()) 
+               date_str = div_fixed.find('p',class_="p-muted-heading")
+               if date_str:
+                  issue_date = parse_date(date_str.text.strip()) 
             #    products = [li.text.strip()  for li in rows[0].find('ul',class_="p-inline-list").find_all("li",class_="p-inline-list__item") ]
                if title_element:
                     title = title_element.text.strip()
-                   
                col = rows[1].find('div',class_="col-8")
                ref = rows[2].find('div',class_="col-8")
                Cves =  ', '.join([li.text.strip() for li in ref.find("ul")])
@@ -108,7 +165,6 @@ def extract_pages(links):
                     for ub in ubuntu:
                       if product.text.strip().startswith(ub):
                         ul = product.find_next()
-                                            
                         for li in  ul.find_all('li',class_="p-list__item"):
                            deblink = li.find_all('a')[1]["href"]
                            if not deblink.startswith('/pro'):
@@ -121,7 +177,10 @@ def extract_pages(links):
                                     'Bulletin Title and Executive Summary':title.split(':')[1],
                                       'Announcement links': link
                               })) 
+                   
     return big_data
+                   
+                                            
 
                 
 def get_amd_link(link):
@@ -184,7 +243,7 @@ def scrape(_data):
 
 def save_data(data):
      df = pd.DataFrame(data)
-     df["Release Date"] = pd.to_datetime(df['Release Date'],format='%d/%m/%Y').dt.date
+     df["Release Date"] = pd.to_datetime(df["Release Date"],format='%d/%m/%Y').dt.date
      folder = 'collected'
      df_sorted = df.sort_values(by='OS')
      file_name = f'Ubuntu-Generated-Month-{patching_date.strftime("%B")}.xlsx'
@@ -195,14 +254,14 @@ def save_data(data):
         
         
 
-def main():
+async def main():
     initialtime  = time.time()
     # extracrt & scraping data 
     links = extract_links()
-    big_data = extract_pages(links)
+    big_data = await extract_pages(links)
     save_data(big_data)
     
     log.info('successful finishing. Time taken: %.2f seconds' ,time.time() - initialtime)
 
 if __name__ == "__main__":
-     main()
+   asyncio.run(main())
